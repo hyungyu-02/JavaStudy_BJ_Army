12f612d878d7fa74b37a80a16e049e8b2a6fd08d
Hyungyu <yona20020209@gmail.com>
2024-03-31 21:56:21 +0900

    Update 11650_좌표_정렬하기.java

--- javaCodes/11650_좌표_정렬하기.java
+++ javaCodes/11650_좌표_정렬하기.java
@@ -1,7 +1,5 @@
-// tip: each public class is put in its own file
 public class main
 {
-    // tip: arguments are passed via the field below this editor
     public static void main(String[] args)
     {
         System.out.println("Hello, world!");
