ff1f93b9fb651345169b8b5fb239d299cf4447ce
Hyungyu <yona20020209@gmail.com>
2024-04-10 22:18:43 +0900

    Create 2609_최대공약수와_최소공배수.java

---
+++ javaCodes/2609_최대공약수와_최소공배수.java
@@ -0,0 +1,31 @@
+//유클리드 호제법(Euclidean algorithm)
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine(), " ");
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = gcd(a, b);
+        System.out.println(c);
+        System.out.println((a*b)/c);
+    }
+
+    public static int gcd(int a, int b){
+        while(b != 0){
+            int r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
+    }
+}
